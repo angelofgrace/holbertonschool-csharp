d4c2dedb529ec0238ecf21e7ad64d63f3903243c
Grace <graceleefallon@gmail.com>
2021-07-12 22:04:39 -0500

    Typo fix

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            double percent = .7953;
+            double percent = .7553;
             double currency = 98765.4321;
             Console.WriteLine("Percent: {0}", percent.ToString("P"));
             Console.WriteLine("Currency: {0}", String.Format("{0:C}", currency));
