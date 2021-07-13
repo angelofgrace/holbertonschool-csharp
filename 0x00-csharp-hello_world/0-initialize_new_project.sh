a15f6436c4a5517275e0503e8fd2cf4a252823c6
Grace <graceleefallon@gmail.com>
2021-07-12 20:16:46 -0500

    First commit

---
+++ 0x00-csharp-hello_world/0-initialize_new_project.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#initialize a new C# project
+
+dotnet new console -n 0-new_project
---
+++ 0x00-csharp-hello_world/1-build_new_project copy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#initialize and build a new C# project
+
+dotnet new console -n 0-new_project
+dotnet build
