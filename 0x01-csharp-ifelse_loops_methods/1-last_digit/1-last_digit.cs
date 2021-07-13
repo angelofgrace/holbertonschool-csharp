9040f51d6c415ff969c97ad6d2fbc679949bae30
Grace <graceleefallon@gmail.com>
2021-07-13 12:28:54 -0500

    Phrase completion based on last digit conditional

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        string phrase = "";
+        if (number % 10 > 5)
+        {
+            phrase = "and is greater than 5";
+        }
+        else if (number == 0)
+        {
+            phrase = "and is 0";
+        }
+        else
+        {
+            phrase = "and is less than 6 and not 0";
+        }
+        Console.WriteLine("The last digit of {0} is {2} {1}", number.ToString(), phrase, (number % 10).ToString());
+    }
+}
