7f7c781fcd8f6696e0374ce61039347b5dabcc09
Grace <graceleefallon@gmail.com>
2021-07-13 12:56:01 -0500

    Iterate through ~most~ of the alphabet

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabet/3-print_alphabet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter;
+            for (letter = 'a'; letter <= 'z'; letter ++)
+            {
+                if (letter == 'q' || letter == 'e')
+                {
+                    continue;
+                }
+                {
+                    Console.Write(letter);
+                }
+            }
+        }
+    }
+}
