18fcb608f6f892d6d7a3ce55ad900a6f824db2f5
Grace <graceleefallon@gmail.com>
2021-07-12 20:53:48 -0500

    Print string maniuplations to stdout

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Holberton School";
+            Console.WriteLine(str, 3);
+            Console.WriteLine(str.Substring(0,9));
+        }
+    }
