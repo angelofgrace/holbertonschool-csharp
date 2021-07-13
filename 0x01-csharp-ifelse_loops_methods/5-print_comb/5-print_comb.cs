f3afed90329deb7a4637e7e8fb5b4867d8f71111
Grace <graceleefallon@gmail.com>
2021-07-13 13:50:45 -0500

    List numbers with specified width, delimiter

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            while (number < 99)
+            {
+                Console.Write("{0:d2}, ", number);
+                number++;
+            }
+            Console.Write("{0}", number);
+        }
+    }
+}
