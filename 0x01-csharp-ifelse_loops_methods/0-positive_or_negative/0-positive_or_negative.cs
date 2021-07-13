adf8463587887bfc8c3e8cf689d3859fb3153c3d
Grace <graceleefallon@gmail.com>
2021-07-13 10:15:46 -0500

    Phrase output based on odd/even conditional

---
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        string phrase = "";
+        if (number > 0)
+        {
+            phrase = "is positive";
+        }
+        else if (number < 0)
+        {
+            phrase = "is negative";
+        }
+        else
+        {
+            phrase = "is zero";
+        }
+        Console.WriteLine("{0} {1}", number, phrase);
+    }
+}
