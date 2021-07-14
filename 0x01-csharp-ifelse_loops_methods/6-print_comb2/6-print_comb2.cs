158a6c1d43498cd2fe54888acb5ff4fbc55cdcb8
Grace <graceleefallon@gmail.com>
2021-07-13 19:00:50 -0500

    Comb through combs of numbers, distinct pairs

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            while (number < 89)
+            {
+                if ((number % 10) > (number  / 10))
+                {
+                    Console.Write("{0:d2}, ", number);
+                }
+                number++;
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
