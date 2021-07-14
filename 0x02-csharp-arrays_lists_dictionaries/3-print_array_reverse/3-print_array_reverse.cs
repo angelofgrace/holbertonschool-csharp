753af7c273ea614dc594f7fd44ca55c65d6136df
Grace <graceleefallon@gmail.com>
2021-07-14 13:13:41 -0500

    Reverse an array with swap algo, account for null

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine("");
+            return;
+        }
+        int y = array.Length - 1;
+        for (int x = 0; x <= array.Length / 2; x++)
+        {
+            int tmp = array[x];
+            array[x] = array[y];
+            array[y] = tmp;
+            y--;
+        }
+        for (int z = 0; z < array.Length; z++)
+        {
+            if (z < array.Length - 1)
+            {
+                Console.Write(array[z] + " ");
+            }
+            else
+            {
+                Console.WriteLine(array[z]);
+            }
+        }
+    }
+}
