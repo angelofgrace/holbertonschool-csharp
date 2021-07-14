62a5fb574cc475fcc06ec657bccb1ab31ac916f2
Grace <graceleefallon@gmail.com>
2021-07-13 19:42:53 -0500

    Fizzy and Buzzy and FizzyBuzzy

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pop = 1;
+            while (pop <= 100)
+            {
+                if (pop % 15 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (pop % 3 == 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (pop % 5 == 0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else
+                {
+                    Console.Write("{0} ", pop);
+                }
+                pop++;
+            }
+            Console.WriteLine("");
+        }
+    }
+}
