1f985fa0ef05cd47a5f7f854e83a9231638fbd96
Grace <graceleefallon@gmail.com>
2021-07-13 19:28:39 -0500

    Loop up to a length to draw a line in console

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        int iter = 0;
+        while (iter < length)
+        {
+            Console.Write("_");
+            iter++;
+        }
+        Console.WriteLine("");
+    }
+}
