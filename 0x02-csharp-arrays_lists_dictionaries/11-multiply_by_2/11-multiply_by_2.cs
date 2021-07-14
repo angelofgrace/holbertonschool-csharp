a01ab9e3ab285e2fe7a53cc47fa63c556e42f11d
Grace <graceleefallon@gmail.com>
2021-07-14 14:29:23 -0500

    Create a copy of a dictionary with modified values

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> results = new Dictionary<string, int>(myDict);
+
+        foreach (string key in myDict.Keys)
+        {
+            results[key] = (results[key] * 2);
+        }
+
+        return (results);
+    }
+}
