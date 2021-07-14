ed47b121c1021e1968c324e858151ab7f74586d5
Grace <graceleefallon@gmail.com>
2021-07-13 19:16:18 -0500

    Function to isolate the last digit of any number

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int rem;
+        rem = number % 10;
+        if (number < 0)
+        {
+            rem = rem * -1;
+        }
+        Console.Write(rem);
+        return rem;
+    }
+}
