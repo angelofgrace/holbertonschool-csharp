05e1cf1eb82d49f0a328fc11fc7ddd10c188945d
Grace <graceleefallon@gmail.com>
2021-07-13 19:36:25 -0500

    Nest loops iterating to length to draw a diagonal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int height, width;
+        for (height = 0; height < length; height++)
+        {
+            for (width = 0; width < height; width++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
+        }
+        Console.WriteLine("");
+    }
+}
