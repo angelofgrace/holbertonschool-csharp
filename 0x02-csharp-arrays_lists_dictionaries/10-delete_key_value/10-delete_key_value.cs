bc29c77cdcf19117504ebb0cf062d5b0e5f6ed0a
Grace <graceleefallon@gmail.com>
2021-07-14 14:16:15 -0500

    Delete a k/v pair from a dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
+    {
+        if (key == null)
+        {
+            return (myDict);
+        }
+
+        myDict.Remove(key);
+
+        return myDict;
+    }
+}
