7a2bd4c7383c353fb8af26be49dc135d9c6fe9e7
Grace <graceleefallon@gmail.com>
2021-07-14 22:23:14 -0500

    Convert dictionary keys to list and sort

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var myList = myDict.Keys.ToList();
+        myList.Sort();
+
+        foreach (string key in myList)
+        {
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+        }
+    }
+}
