89e8c2d3997b2a089283fd6b0a24f6db6c855db5
Grace <graceleefallon@gmail.com>
2021-07-15 13:14:55 -0500

    Read and manipulate a stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        // count the stack
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+
+        // look at the top card of your library
+        if (aStack == null)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        Console.WriteLine("Top item: {0}", aStack.Peek());
+
+        // search for a needle in the haySTACK
+        bool found = aStack.Contains(search);
+        Console.WriteLine("Stack contains {0}: {1}", search, found);
+        if (found)
+        {
+            string check = "";
+            while (check != search)
+            {
+                check = aStack.Pop();
+            }
+        }
+
+        // add to the pile
+        aStack.Push(newItem);
+        return (aStack);
+    }
+}
