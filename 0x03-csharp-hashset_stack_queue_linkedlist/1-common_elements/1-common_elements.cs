a54a4fbe9c5909d0e939415e4fb25bfc2b7ef2e8
Grace <graceleefallon@gmail.com>
2021-07-15 11:52:40 -0500

    Find the common elements of two lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> commonalityList = new List<int>();
+
+        foreach (int element in list1)
+        {
+            if (list2.Contains(element))
+            {
+                commonalityList.Add(element);
+            }
+        }
+
+        return (commonalityList);
+    }
+}
