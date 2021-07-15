0627ba4e774cc941101c61588433e815795c4034
Grace <graceleefallon@gmail.com>
2021-07-14 23:23:49 -0500

    Initialize and format a two-dimensional array

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] multiDimensional = new int [5,5]
+            {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0}
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (j < 4)
+                    {
+                        Console.Write(multiDimensional[i,j] + " ");
+                    }
+                    else
+                    {
+                        Console.Write(multiDimensional[i,j]);
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
