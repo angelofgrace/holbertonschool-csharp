fae6574956d2a18ed171ed980f325f6b0df200fa
Grace <graceleefallon@gmail.com>
2021-07-14 23:02:24 -0500

    Account for dictionary with no values

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -6,6 +6,10 @@ class Dictionary
 {
     public static string BestScore(Dictionary<string, int> myList)
     {
+        if (myList.Values.Count == 0)
+        {
+            return ("None");
+        }
         List<KeyValuePair<string, int>> itemList = new List<KeyValuePair<string, int>>(myList);
         itemList.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
         return (itemList[itemList.Count - 1].Key);
