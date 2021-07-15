21edfb78eb96de66ecf9532f51513b9144fe93a9
Grace <graceleefallon@gmail.com>
2021-07-15 17:51:59 -0500

    Find a value within a linked list the hard way

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        if (myLList.Contains(value) != true || myLList.Count == 0)
+        {
+            return (-1);
+        }
+
+        for (int x = 0; x < myLList.Count; x++)
+        {
+            if (myLList.ElementAt(x) == value)
+            {
+                return (x);
+            }
+        }
+
+        return (-1);
+    }
+}
