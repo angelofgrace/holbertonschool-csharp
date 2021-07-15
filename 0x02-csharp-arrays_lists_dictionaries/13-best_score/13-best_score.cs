e765f10768153c2885cbc8477c496466c626f7fb
Grace <graceleefallon@gmail.com>
2021-07-14 22:58:06 -0500

    Convert dict to list of kvps, sort by values

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        List<KeyValuePair<string, int>> itemList = new List<KeyValuePair<string, int>>(myList);
+        itemList.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+        return (itemList[itemList.Count - 1].Key);
+    }
+}
