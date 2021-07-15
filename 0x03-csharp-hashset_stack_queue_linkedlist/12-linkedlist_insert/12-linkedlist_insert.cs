8cab25a48b0aa4e44d742c2451c3e37caeafa428
Grace <graceleefallon@gmail.com>
2021-07-15 18:53:55 -0500

    Add a value to a sorted linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current;
+
+        if (myLList.First == null || myLList.First.Value >= n)
+        {
+            myLList.AddFirst(n);
+        }
+        current = myLList.First;
+        while (current.Next != null && current.Next.Value < n)
+        {
+            current = current.Next;
+        }
+        myLList.AddAfter(current, n);
+        current = current.Next;
+        return (current);
+    }
+}
