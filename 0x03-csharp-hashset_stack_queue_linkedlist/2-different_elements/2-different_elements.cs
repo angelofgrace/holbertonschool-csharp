0c4d1e6f1a7f28eec0e628c3f513132c2f0553df
Grace <graceleefallon@gmail.com>
2021-07-15 12:18:57 -0500

    Find the unique elements of two lists and sort

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        // short of writing an entire binary search algo, 
+        // given the library and method limitations,
+        // this is as efficient as it gets in 15 minutes of code
+
+        List<int> distinctList = new List<int>();
+        List<int> commonList = new List<int>();
+
+        foreach (int element in list1)
+        {
+            if (list2.Contains(element) != true)
+            {
+                distinctList.Add(element);
+            }
+            else
+            {
+                commonList.Add(element);
+            }
+        }
+        foreach (int element in list2)
+        {
+            if (commonList.Contains(element) != true)
+            {
+                distinctList.Add(element);
+            }
+        }
+        distinctList.Sort();
+        return (distinctList);
+    }
+}
