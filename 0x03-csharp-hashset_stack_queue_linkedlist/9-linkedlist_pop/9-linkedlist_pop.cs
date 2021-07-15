ee47d1de7e3080fedc8f591ae733291bd6c2685b
Grace <graceleefallon@gmail.com>
2021-07-15 18:02:21 -0500

    Remove the first node of a linked list, store its value

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count == 0)
+        {
+            return (0);
+        }
+
+        
+        LinkedListNode<int> top = myLList.First;
+        myLList.RemoveFirst();
+        return (top.Value);        
+    }
+}
