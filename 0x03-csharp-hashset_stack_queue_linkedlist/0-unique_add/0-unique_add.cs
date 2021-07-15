cf42553c788ec9cf5ad6daceb51405a44880922e
Grace <graceleefallon@gmail.com>
2021-07-15 11:37:10 -0500

    Add all unique elements of a list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        List<int> uniqueElements = new List<int>();
+        int total = 0;
+
+        foreach (int number in myList)
+        {
+            if (uniqueElements.Contains(number) != true)
+            {
+                uniqueElements.Add(number);
+                total = total + number;
+            }
+        }
+        return total;
+    }
+}
