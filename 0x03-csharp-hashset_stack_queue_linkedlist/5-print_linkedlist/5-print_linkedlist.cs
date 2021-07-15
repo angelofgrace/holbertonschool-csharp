0270f12c129703f49e1eaec415d7d4367ae5e470
Grace <graceleefallon@gmail.com>
2021-07-15 17:27:16 -0500

    Create and Print a linked list of integers

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> newList = new LinkedList<int>();
+        
+        if (size < 0)
+        {
+            return (newList);
+        }
+
+        for (int x = 0; x < size; x++)
+        {
+            newList.AddLast(x);
+            Console.WriteLine(x);
+        }
+
+        return (newList);
+
+    }
+}
