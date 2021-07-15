7d2663a3d57463cc90bbf0ea460b7745f873715f
Grace <graceleefallon@gmail.com>
2021-07-15 17:40:39 -0500

    Add a node to the beginning of a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> Add(LinkedList<int> myLList, int n)
+    {
+        myLList.AddFirst(n);
+        return (myLList);
+    }
+}
