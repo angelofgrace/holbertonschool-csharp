60d35530b45b1d28708c8c841d6705a3b40a88db
Grace <graceleefallon@gmail.com>
2021-07-14 23:58:52 -0500

    Square a Matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int rows = myMatrix.GetLength(0);
+        int columns = myMatrix.GetLength(1);
+
+        int[,] newMatrix = new int[rows, columns];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                int tmp = Convert.ToInt32(myMatrix[i,j]);
+                double result = (Math.Pow(tmp, 2));
+                int tmpSqr = Convert.ToInt32(result);
+                newMatrix.SetValue(tmpSqr, i, j);
+            }
+        }
+        
+        return (newMatrix);
+    }
+}
