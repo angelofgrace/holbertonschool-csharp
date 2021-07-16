1a7eb761237028b6c48d863aaaa367463a7aa80d
Grace <graceleefallon@gmail.com>
2021-07-15 19:37:19 -0500

    Account for index 0 input

--- 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -6,7 +6,7 @@ class LList
 {
     public static void Delete(LinkedList<int> myLList, int index)
     {
-        if (index < myLList.Count && index > 0)
+        if (index < myLList.Count && index >= 0)
         {
         int find = myLList.ElementAt(index);
         myLList.Remove(find);
