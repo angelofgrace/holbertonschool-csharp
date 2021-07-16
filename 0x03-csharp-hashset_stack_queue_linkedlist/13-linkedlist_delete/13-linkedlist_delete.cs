ae5d5ca794dd798d1c77f660cd6cfc0b26e193d5
Grace <graceleefallon@gmail.com>
2021-07-15 19:31:26 -0500

    Remove the element at a given index of a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int find = myLList.ElementAt(index);
+        myLList.Remove(find);
+    }
+}
