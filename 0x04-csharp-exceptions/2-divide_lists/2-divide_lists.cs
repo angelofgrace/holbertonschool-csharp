97fffc1a7a4b7c395d7c5919ac46f698a26bf057
Grace <graceleefallon@gmail.com>
2021-07-19 17:35:56 -0500

    Divide a list by another list, handle exceptions

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> result = new List<int>();
+        for (int x = 0; x < listLength; x++)
+        {
+            try
+            {
+                result.Add(list1[x] / list2[x]);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+            catch (DivideByZeroException)
+            {
+                result.Add(0);
+                Console.WriteLine("Cannot divide by zero");
+            }
+        }
+        return (result);
+    }
+}
