df9fd7fb1e47a5ba6183bfca75e31d30a36c2024
Grace <graceleefallon@gmail.com>
2021-07-19 17:17:21 -0500

    Catch an out of range exception in a for loop

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        for (int x = 0; x < n; x++)
+        {
+            try
+            {
+                Console.WriteLine(myList[x]);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return (x);
+            }
+        }
+        return (n);
+    }
+}
