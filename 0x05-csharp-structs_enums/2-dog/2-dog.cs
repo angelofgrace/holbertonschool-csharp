d6d3e809cc6b9ce2c0e00a92af326a74eaaad4f6
Grace <graceleefallon@gmail.com>
2021-07-20 12:19:37 -0500

    Construct a dog data structure

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog 
+{
+    public Dog(string Name, float Age, string Owner, Rating Rating)
+    {
+        name = Name;
+        age = Age;
+        owner = Owner;
+        rating = Rating;
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
