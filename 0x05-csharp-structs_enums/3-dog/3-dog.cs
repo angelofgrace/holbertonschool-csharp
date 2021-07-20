a2000af558fa043eb43dc0ba9e2eb4409859ac69
Grace <graceleefallon@gmail.com>
2021-07-20 12:26:46 -0500

    Override toString to return all attrs and values

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog 
+{
+    public Dog(string Name, float Age, string Owner, Rating Rating)
+    {
+        name = Name;
+        age = Age;
+        owner = Owner;
+        rating = Rating;
+    }
+
+    public override string ToString()
+    {
+        return ("Dog Name: " + name.ToString() + "\n" +
+                "Age: " + age.ToString() + "\n" +
+                "Owner: " + owner.ToString() + "\n" +
+                "Rating: " + rating.ToString());
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
