e84d4367efe43a96c9b120161feba40d933f6b2c
Grace <graceleefallon@gmail.com>
2021-07-30 14:35:13 -0500

    use getvalue as a method

--- 0x06-csharp-classes/3-enemy/3-enemy.cs
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -15,7 +15,7 @@ namespace Enemies
             {
                 throw new ArgumentException("Health must be greater than or equal to 0");
             }
-            this.health = value.GetValueOrDefault;
+            this.health = value.GetValueOrDefault();
         }
 
         /// <summary> Health Point count retriever </summary>
