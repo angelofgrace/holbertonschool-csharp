23b37e44828317a7ea631e401eeb594f99e63df7
Grace <graceleefallon@gmail.com>
2021-07-30 11:11:02 -0500

    Gethealth public method for zombies

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Enemies
+{ 
+    /// <summary> Do you know the enemy? </summary>
+    public class Zombie
+    {        
+        /// <summary> Health Points counter </summary>
+        public int health;
+        
+        /// <summary> Do you know your enemy? </summary>
+        public Zombie(int value = 0)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+
+        public int GetHealth()
+        {
+            return this.health;
+        }
+
+    }
+}
