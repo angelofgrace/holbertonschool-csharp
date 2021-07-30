97059ca5c99aa79b6e51a73603aa6f3615603f08
Grace <graceleefallon@gmail.com>
2021-07-30 09:42:36 -0500

    Add public field and constructor

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Enemies
+{ 
+    /// <summary> Do you know your enemy? </summary>
+    public class Zombie
+    {
+        public Zombie(int health = 0);
+        public int health;
+    }
+}
