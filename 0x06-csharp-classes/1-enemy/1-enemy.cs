9c3fedf69592e21722a49eda791c462e47acca14
Grace <graceleefallon@gmail.com>
2021-07-30 10:04:08 -0500

    ordering of fields in zombie class

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -4,13 +4,15 @@ namespace Enemies
 { 
     /// <summary> Do you know the enemy? </summary>
     public class Zombie
-    {
-        /// <summary> Do you know your enemy? </summary>
-        public Zombie(int health)
-        {
-            this.health = 0;
-        }
+    {        
         /// <summary> Health Points counter </summary>
         public int health;
+        
+        /// <summary> Do you know your enemy? </summary>
+        public Zombie(int health = 0)
+        {
+            this.health = health;
+        }
+
     }
 }
