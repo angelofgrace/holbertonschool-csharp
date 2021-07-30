2450cba23f448fda00b34f96702d519836cdc8a2
Grace <graceleefallon@gmail.com>
2021-07-30 15:27:43 -0500

    get and set a new field, zombie name

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enemies
+{ 
+    /// <summary> Do you know the enemy? </summary>
+    public class Zombie
+    {        
+        /// <summary> Health Points counter </summary>
+        public int health;
+
+        /// <summary> Name value </summary>
+        public string name = "(No name)";
+
+        /// <summary> getter/setter property for name </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+        /// <summary> Do you know your enemy? </summary>
+        public Zombie(int? value = 0)
+        {
+            if (value < 0 || value.HasValue == false)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value.GetValueOrDefault();
+        }
+
+        /// <summary> Health Point count retriever </summary>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+
+    }
+}
