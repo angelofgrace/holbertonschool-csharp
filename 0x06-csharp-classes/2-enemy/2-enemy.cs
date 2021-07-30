8ce3aa4c36d5532365aae6ca6bbb5202c65c7274
Grace <graceleefallon@gmail.com>
2021-07-30 10:21:43 -0500

    Add constructor with a value not less than 0

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Enemies
+{ 
+    /// <summary> Do you know the enemy? </summary>
+    public class Zombie
+    {        
+        /// <summary> Health Points counter </summary>
+        public int health;
+        
+        /// <summary> Do you know your enemy? </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = 0;
+        }
+
+    }
+}
