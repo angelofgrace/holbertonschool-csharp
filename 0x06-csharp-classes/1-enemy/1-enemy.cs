c4084c656f822071f9e9d9025298e5035a94d947
Grace <graceleefallon@gmail.com>
2021-07-30 09:43:50 -0500

    Declare a body for the constructor

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -5,7 +5,9 @@ namespace Enemies
     /// <summary> Do you know your enemy? </summary>
     public class Zombie
     {
-        public Zombie(int health = 0);
+        public Zombie(int health = 0)
+        {          
+        }
         public int health;
     }
 }
