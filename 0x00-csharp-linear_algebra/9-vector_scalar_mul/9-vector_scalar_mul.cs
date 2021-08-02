a66b5e701a9725c071c1f35f5d21bbb064b837d6
Grace <graceleefallon@gmail.com>
2021-08-02 17:49:29 -0500

    Multiply any 2 or 3D vector by a scalar

---
+++ 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary> Vector Math Class </summary>
+class VectorMath
+{
+    /// <summary> Multiply a vector of 2 or 3 dimensions by a scalar </summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] productVector = new double[vector.Length];
+        if (vector.Length > 3 || vector.Length < 2)
+        {
+            productVector[0] = -1;
+            return productVector;
+        }
+        for (int x = 0; x < vector.Length; x++)
+        {
+            productVector[x] = vector[x] * scalar;
+        }
+        return productVector;
+    }
+}
