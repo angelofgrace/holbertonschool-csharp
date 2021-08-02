72179abd64dab7dc9ddce417ff776f965c80b92c
Grace <graceleefallon@gmail.com>
2021-08-02 13:19:05 -0500

    use Math to compute a 2 or 3 dimensional vector's magnitude

---
+++ 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        int givenSides = 0;
+        if (vector.count < 2 || vector.count > 3)
+        {
+            return (-1);
+        }
+        foreach (double number in vector)
+        {
+            givenSides = givenSides + (number * number);
+        }
+        return Math.Round(Math.Sqrt(givenSides), 2);
+    }
+}
