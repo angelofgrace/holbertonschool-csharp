a87961116130cecf052d1f93a971a220a49a0eef
Grace <graceleefallon@gmail.com>
2021-08-02 17:16:17 -0500

    XML documentation

--- 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 
+/// <summary> Vector Math class </summary>
 class VectorMath
 {
+    /// <summary> Script to calculate the magnitude of any 2 or 3D vector </summary>
     public static double Magnitude(double[] vector)
     {
         double givenSides = 0;
