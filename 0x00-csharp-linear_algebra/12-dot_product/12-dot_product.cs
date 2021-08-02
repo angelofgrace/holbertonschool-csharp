7fe24798ace5ebbca54be6c32e62f4dc0ee37ec1
Grace <graceleefallon@gmail.com>
2021-08-02 18:44:04 -0500

    Find dot product of any two 2/3D vectors

---
+++ 0x00-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary> Vector Math class </summary>
+class VectorMath
+{
+    /// <summary> find the dot product of two vectors, either 2D or 3D </summary>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        int l1, l2;
+        l1 = vector1.Length;
+        l2 = vector2.Length;
+        if (l1 < 2 || l1 > 3 || l2 < 2 || l2 > 3 || l1 != l2)
+        {
+            return (-1);
+        }
+        list<double> products = new list<double>();
+        for (int x = 0; x < l1; x++)
+        {
+            products.Add(vector1[x] * vector2[x]);
+        }
+        double result = products.Sum;
+        return result;
+    }
+}
