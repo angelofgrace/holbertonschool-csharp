9c1804cfbba7b9f79d901c67cc1653025df482c4
Grace <graceleefallon@gmail.com>
2021-08-03 18:44:34 -0500

    Add logic to track head and tail nodes for correct sequencing

--- 0x01-csharp-generics/1-enqueue/queue.cs
+++ 0x01-csharp-generics/1-enqueue/queue.cs
@@ -12,8 +12,8 @@ class Queue<T>
     /// <summary> A node of a Linked List </summary>
     public class Node
     {
-        T value = default(T);
-        Node next = null;
+        public T value = default(T);
+        public Node next = null;
 
         public Node(T val = default(T))
         {
@@ -21,8 +21,8 @@ class Queue<T>
         }
     }
 
-    public Node head;
-    public Node tail;
+    public Node head = null;
+    public Node tail = null;
     public int count = 0;
 
     public void Enqueue(T val)
@@ -31,6 +31,12 @@ class Queue<T>
         if (count == 0)
         {
             head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
         }
         count += 1;
     }
