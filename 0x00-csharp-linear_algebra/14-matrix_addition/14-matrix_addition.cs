802b092de50ed77a87f80979d00fbd089a1c2cba
Grace <graceleefallon@gmail.com>
2021-08-02 21:39:57 -0500

    use getLength to measure multidimensional array

--- 0x00-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x00-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -8,24 +8,24 @@ class MatrixMath
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
         int l1, l2;
-        l1 = matrix1.Length;
-        l2 = matrix2.Length;
+        l1 = matrix1.GetLength(0);
+        l2 = matrix2.GetLength(0);
         if (l1 != l2 || l1 == 0)
         {
             double[,] edgeMatrix = new double[1,1];
             edgeMatrix[0,0] = -1;
             return edgeMatrix;
         }
-        else if (matrix1[0].Length > 3 || matrix1[0].Length < 2 || matrix2[0].Length < 2 || matrix2[0].Length > 3)
+        else if (matrix1.GetLength(1) > 3 || matrix1.GetLength(1) < 2 || matrix2.GetLength(1) < 2 || matrix2.GetLength(1) > 3)
         {
             double[,] edgeMatrix = new double [1,1];
             edgeMatrix[0,0] = -1;
             return edgeMatrix;
         }
-        double[,] sumMatrix = new double[matrix1.Length, matrix1[0].Length];
-        for (int x = 0; x < matrix1.Length; x++)
+        double[,] sumMatrix = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
+        for (int x = 0; x < matrix1.GetLength(0); x++)
         {
-            for (int y = 0; y < matrix1[0].Length; y++)
+            for (int y = 0; y < matrix1.GetLength(1); y++)
             {
                 sumMatrix[x,y] = matrix1[x,y] + matrix2[x,y];
             }
