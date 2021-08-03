a88738f8c68a964987e6633267bafd17ede2d9ff
Grace <graceleefallon@gmail.com>
2021-08-03 16:20:28 -0500

    Checker lied about requested file name

--- 0x01-csharp-generics/0-queue/Program.cs
+++ 0x01-csharp-generics/0-queue/Program.cs
@@ -3,6 +3,7 @@
 /// <summary> Custom Queue class </summary>
 class Queue<T>
 {
+    /// <summary> returns the type of an object </summary>
     public static Type CheckType()
     {
         return this.GetType();
