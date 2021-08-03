b79dad664d4563424f45b5db7615b171e670413b
Grace <graceleefallon@gmail.com>
2021-08-03 18:57:44 -0500

    peek at the head value w/o removing the node

---
+++ 0x01-csharp-generics/3-peek/queue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+/// <summary> Custom Queue class </summary>
+class Queue<T>
+{
+    /// <summary> returns the type of an object </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary> A node of a Linked List </summary>
+    public class Node
+    {
+        public T value = default(T);
+        public Node next = null;
+
+        public Node(T val = default(T))
+        {
+            value = val;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+    public int count = 0;
+
+    public void Enqueue(T val)
+    {
+        Node newNode = new Node(val);
+        if (count == 0)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count += 1;
+    }
+
+    public T Dequeue()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+            T headValue = head.value;
+            head = head.next;
+            count -= 1;
+            return headValue;
+    }
+
+    public T Peek()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        return head.value;        
+
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+}
