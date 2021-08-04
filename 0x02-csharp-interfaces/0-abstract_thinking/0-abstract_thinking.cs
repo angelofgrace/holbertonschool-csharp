5ba8a88631e94d84f3c12251dbef065e480db135
Grace <graceleefallon@gmail.com>
2021-08-04 16:44:15 -0500

    fix return string formatting issues in override

--- 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 
+/// <summary> abstract class for general structure of derived classes </summary>
 public abstract class Base
 {
+    /// <summary> defining title of an instance </summary>
     public string name;
 
-    public override ToString()
+    /// <summary> basic ToString override </summary>
+    public override string ToString()
     {
-        return String.Format("{} is a {}", name, typof(name));
+        return String.Format("{0} is a {1}", name, this.GetType().Name);
     }
 }
