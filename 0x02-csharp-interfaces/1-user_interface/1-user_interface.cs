d1ae086465c364ffd84154e634402f9998bb8396
Grace <graceleefallon@gmail.com>
2021-08-04 16:58:44 -0500

    Create and inherit from three interfaces of base class

---
+++ 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+/// <summary> abstract class for general structure of derived classes </summary>
+public abstract class Base
+{
+    /// <summary> defining title of an instance </summary>
+    public string name;
+
+    /// <summary> basic ToString override </summary>
+    public override string ToString()
+    {
+        return String.Format("{0} is a {1}", name, this.GetType().Name);
+    }
+
+    /// <summary> interactivity indicator </summary>
+    public interface IInteractive
+    {
+        void Interact();
+    }
+
+    /// <summary> breakability indicator </summary>
+    public interface IBreakable
+    {
+        int durability
+        {
+            get {}
+            set {}
+        }
+        void Break();
+    }
+
+    /// <summary> collectibility indicator </summary>
+    public interface ICollectable
+    {
+        bool isCollected
+        {
+            get {}
+            set {}
+        }
+        void Collect();
+    }
+}
+
+/// <summary> Testing 1.. 2.. 3.. 4.. </summary>
+public class TestObject : Base
+{
+    public TestObject() {}
+}
+
