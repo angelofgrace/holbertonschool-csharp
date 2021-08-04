8d49f4bcd7b5a2568522b443db5da804feb45243
Grace <graceleefallon@gmail.com>
2021-08-03 19:16:51 -0500

    Print each Node's values to stdout

---
+++ 0x01-csharp-generics/4-print/queue.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+/// <summary> Custom Queue class </summary>
+class Queue<T>
+{
+    /// <summary> returns the type of an object </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary> A node of a Linked List </summary>
+    public class Node
+    {
+        public T value = default(T);
+        public Node next = null;
+
+        public Node(T val = default(T))
+        {
+            value = val;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+    public int count = 0;
+
+    public void Enqueue(T val)
+    {
+        Node newNode = new Node(val);
+        if (count == 0)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count += 1;
+    }
+
+    public T Dequeue()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+            T headValue = head.value;
+            head = head.next;
+            count -= 1;
+            return headValue;
+    }
+
+    public T Peek()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        return head.value;        
+
+    }
+
+    public void Print()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            Node tmpNode = head;
+            while (tmpNode.next != null)
+            {
+                Console.WriteLine(tmpNode.value);
+                tmpNode = tmpNode.next;
+            }
+            Console.WriteLine(tail.value);
+        }
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+}
