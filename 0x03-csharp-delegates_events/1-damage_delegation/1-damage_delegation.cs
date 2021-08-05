bcc18836e5f3b3ed8c2a28c0698a6003367515ef
Grace <graceleefallon@gmail.com>
2021-08-05 17:23:15 -0500

    delegate calchealth and dmg/heal assoc methods

---
+++ 0x03-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+/// <summary> player character data tracker </summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// each player gets one of these
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp > 0)
+        {
+            this.maxHp = maxHp;
+            this.hp = maxHp;
+        }
+        else
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+            this.hp = 100f;
+        }
+    }
+
+    /// how much health does the player have?
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", name, maxHp, hp);
+    }
+
+    /// changes in health from various sources
+    public delegate void CalculateHealth(float healthPoints);
+
+    /// damage reduces hit points
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+        {
+            damage = 0;
+        }
+        Console.WriteLine("{0} takes {1} damage!", name, damage);
+        hp -= damage;
+    }
+
+    /// healing increases hit points
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+        {
+            heal = 0;
+        }
+        Console.WriteLine("{0} heals {1} HP!", name, heal);
+        hp += heal;
+    }
+}
